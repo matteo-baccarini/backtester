631f16ecfa275cf61e80cc1a4a02b710cb4e5847
Matteo Baccarini <baccarinimatteo5@gmail.com>
2025-10-23 23:23:22 -0400

    strategy-engine file addition, main algorithms for financial aspects of trading platform

---
+++ packages/strategy-engine/src/indicators/index.ts
@@ -0,0 +1,7 @@
+// Technical Indicators
+export * from "./types";
+export * from "./sma";
+export * from "./ema";
+export * from "./macd";
+export * from "./rsi";
+export * from "./bollinger-bands";
---
+++ packages/strategy-engine/src/indicators/types.ts
@@ -0,0 +1,8 @@
+export interface OHLCV {
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  timestamp: Date;
+}
