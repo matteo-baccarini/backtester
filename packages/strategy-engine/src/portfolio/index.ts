061e4d5df98bc4f82b480ea038df933995833f39
matteo-baccarini <baccarinimatteo5@gmail.com>
2025-10-27 15:00:49 -0400

    Portfolio added

---
+++ packages/strategy-engine/src/portfolio/index.ts
@@ -0,0 +1,19 @@
+export class Portfolio {
+  constructor(initialCapital: number) {}
+  getValue(): number {
+    return 0;
+  }
+  getPositions(): any[] {
+    return [];
+  }
+  getCash(): number {
+    return 0;
+  }
+  addPosition(symbol: string, quantity: number, price: number): boolean {
+    return false;
+  }
+  removePosition(symbol: string, quantity: number, price: number): boolean {
+    return false;
+  }
+  reset(): void {}
+}
