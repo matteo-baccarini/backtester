7d6fb907afaf3b0073fc6dd498b7e3c3cc6182ce
matteo-baccarini <baccarinimatteo5@gmail.com>
2025-11-02 12:14:26 -0500

    RSI class implementation initialized, created initial function

--- packages/strategy-engine/src/indicators/rsi.ts
+++ packages/strategy-engine/src/indicators/rsi.ts
@@ -1,10 +1,46 @@
-import { OHLCV } from "./types";
+import { IndicatorResult, OHLCV } from "./types";
 
 export class RSI {
-  constructor(period: number = 14) {}
-  calculate(data: OHLCV[]): any[] {
-    return [];
+  private period : number;
+
+  constructor(period: number = 14) {
+    this.period = period;
   }
+
+  private hasEnoughData(data : OHLCV[]): boolean {
+    return data.length >= 14;
+  }
+
+  private calculateGainLoss(data: OHLCV[]): { avgGain: number; avgLosses: number } {
+    let gains: number = 0;
+    let losses : number = 0;
+
+    for (let i = 1; i < data.length; i++) {
+      const change = data[i].close - data[i - 1].close;
+      if (change > 0) {
+        gains += change;
+      } else {
+        losses += Math.abs(change);
+      }
+    }
+
+    return { avgGain : gains/this.period , avgLosses : losses/this.period };
+  }
+  
+  // calculate(data: OHLCV[]): IndicatorResult[] {
+  //   if (!this.hasEnoughData(data)) {
+  //     return [];
+  //   }
+
+  //   const results : IndicatorResult[] = [];
+  //   const {avgGain, avgLosses} = this.calculateGainLoss(data.slice(0, this.period + 1));
+
+  //   let currentAvgGain = avgGain;
+  //   let currentAvgLosses = avgLosses;
+
+
+  // }
+
   getValue(): number | null {
     return null;
   }
