fb6d405cd7fee3832b487ca17adc223448e54ac3
matteo-baccarini <baccarinimatteo5@gmail.com>
2025-11-10 22:46:08 -0500

    first strategy implementation and subject to revision

--- packages/strategy-engine/src/strategies/SMACrossoverStrategy.ts
+++ packages/strategy-engine/src/strategies/SMACrossoverStrategy.ts
@@ -1,6 +1,6 @@
 import {Signal, IsStrategy, SMA, OHLCV, Portfolio} from '../index'
 
-class SMACrossoverStrategy implements IsStrategy{
+export class SMACrossoverStrategy implements IsStrategy{
     private symbol : string;
     
     private shortSMA : SMA;
@@ -13,7 +13,52 @@ class SMACrossoverStrategy implements IsStrategy{
     }
 
     onBar(price : OHLCV, portfolio : Portfolio) : Signal {
-        this.shortSMA.update(price);
-        this.longSMA.update(price);
+        const shortValue = this.shortSMA.update(price);
+        const longValue = this.longSMA.update(price);
+
+        if (shortValue == null || longValue == null || shortValue.value == null || longValue.value == null){
+            return {
+                action : 'HOLD',
+                symbol : this.symbol,
+                confidence : 0,
+                reason : "SMA values not yet calculated",
+                timestamp : price.timestamp,
+            }
+        }
+
+        const position = portfolio.getPosition(this.symbol);
+
+        if (shortValue.value > longValue.value && !position) {
+            return {
+                action : 'BUY',
+                symbol : this.symbol,
+                confidence : (shortValue.value - longValue.value) / longValue.value,
+                reason : `SMA(${shortValue}) crossed above SMA(${longValue})`,
+                timestamp : price.timestamp,
+            }
+        }
+
+        if (shortValue.value < longValue.value && position){
+            return {
+                action : 'SELL',
+                symbol : this.symbol,
+                confidence : (longValue.value - shortValue.value) / longValue.value,
+                reason : `SMA(${shortValue}) crossed below SMA(${longValue})`,
+                timestamp : price.timestamp,
+            }
+        }
+
+        return {
+            action : 'HOLD',
+            symbol : this.symbol,
+            confidence : 0,
+            reason : 'Hold for future values',
+            timestamp : price.timestamp,
+        }
+    }
+
+    reset() : void {
+        this.longSMA.reset();
+        this.shortSMA.reset();
     }
 }
