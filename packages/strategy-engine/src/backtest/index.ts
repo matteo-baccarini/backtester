122681bc39700edf6a2dce8cb76f54440b94f19e
matteo-baccarini <baccarinimatteo5@gmail.com>
2025-11-11 23:21:02 -0500

    initial implementation of backtest engine

--- packages/strategy-engine/src/backtest/index.ts
+++ packages/strategy-engine/src/backtest/index.ts
@@ -1,10 +1,27 @@
+// export class BacktestEngine {
+//   constructor(initialCapital: number) {}
+//   run(strategy: any, data: any[]): any {
+//     return {};
+//   }
+//   getResults(): any {
+//     return {};
+//   }
+//   reset(): void {}
+// }
+
+import { OHLCV } from "../indicators";
+import { IsStrategy } from "../strategies";
+
 export class BacktestEngine {
-  constructor(initialCapital: number) {}
-  run(strategy: any, data: any[]): any {
-    return {};
+  private initialCapital : number;
+  private strategyInstance : IsStrategy;
+  private historicalData : OHLCV[];
+
+  constructor(inputCash : number, strategy : IsStrategy, historicalData : OHLCV[]) {
+    this.historicalData = historicalData;
+    this.initialCapital = inputCash;
+    this.strategyInstance = strategy;
   }
-  getResults(): any {
-    return {};
-  }
-  reset(): void {}
+
+  
 }
