e09c45bdb80b299dc22bcb4d1c8ed9e85429fbca
matteo-baccarini <baccarinimatteo5@gmail.com>
2025-11-11 23:29:34 -0500

    updated backtestEngine constructor

--- packages/strategy-engine/src/backtest/index.ts
+++ packages/strategy-engine/src/backtest/index.ts
@@ -10,18 +10,23 @@
 // }
 
 import { OHLCV } from "../indicators";
+import { Portfolio } from "../portfolio";
 import { IsStrategy } from "../strategies";
 
 export class BacktestEngine {
-  private initialCapital : number;
+  private portfolio : Portfolio;
   private strategyInstance : IsStrategy;
   private historicalData : OHLCV[];
 
   constructor(inputCash : number, strategy : IsStrategy, historicalData : OHLCV[]) {
     this.historicalData = historicalData;
-    this.initialCapital = inputCash;
+    this.portfolio = new Portfolio(inputCash);
     this.strategyInstance = strategy;
   }
 
-  
+  runEngine() {
+    for (let i : number = 0; i < this.historicalData.length; i++){
+      this.strategyInstance.onBar(this.historicalData[i])
+    }
+  }
 }
