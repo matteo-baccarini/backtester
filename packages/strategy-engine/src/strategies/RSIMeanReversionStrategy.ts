dddc8a7417b060bc5894d674b3251a95e9ca0a34
matteo-baccarini <baccarinimatteo5@gmail.com>
2025-11-17 20:13:29 -0500

    implemented RSI meanCrossoverStrategy, might need some perfectioning

--- packages/strategy-engine/src/strategies/RSIMeanReversionStrategy.ts
+++ packages/strategy-engine/src/strategies/RSIMeanReversionStrategy.ts
@@ -1,4 +1,4 @@
-import { IsStrategy, RSI, OHLCV, Portfolio, Signal } from '../index';
+import {IndicatorResult, IsStrategy, RSI, OHLCV, Portfolio, Signal } from '../index';
 
 export class RSIStrategy implements IsStrategy {
     ///usually calculates RSI over 14 day period
@@ -11,12 +11,85 @@ export class RSIStrategy implements IsStrategy {
     private symbol : string;
     private oversoldThreshold : number;
     private overboughtThreshold : number;
-    private previousRSI : number | null = null;
+    private timeStop : number;
+    private timeLeft : number;
 
-    constructor(symbol : string, RSIPeriod : number, oversold : number = 30, overbought : number = 70){
+    constructor(symbol : string, RSIPeriod : number, oversold : number = 30, overbought : number = 70, timeStop : number = 5){
         this.symbol = symbol;
         this.RSI = new RSI(RSIPeriod);
         this.overboughtThreshold = overbought;
         this.oversoldThreshold = oversold;
+        this.timeStop = timeStop;
+        this.timeLeft = timeStop;
+    }
+
+    onBar(price : OHLCV, portfolio : Portfolio) : Signal {
+        const rsiResult : IndicatorResult | null = this.RSI.update(price);
+
+        if (!rsiResult || rsiResult.value === null){
+            return {
+                action : 'HOLD',
+                symbol : this.symbol,
+                confidence : 0,
+                reason : 'RSI not ready -> warming up',
+                timestamp : price.timestamp,
+            };
+        }
+
+        const rsiValue : number = rsiResult.value;
+        const hasPosition = portfolio.getPosition(this.symbol);
+
+        if (hasPosition){
+            this.timeLeft -= 1;
+
+            if (rsiValue > this.overboughtThreshold){
+                this.timeLeft = this.timeStop;
+                
+                return {
+                    symbol : this.symbol,
+                    action : 'SELL',
+                    confidence : 0,
+                    reason : 'Position is overbought -> RSI > 70',
+                    timestamp : price.timestamp,
+                };
+            }
+
+            if (this.timeLeft === 0){
+                this.timeLeft = this.timeStop;
+
+                return {
+                    symbol : this.symbol,
+                    action : 'SELL',
+                    confidence : 0,
+                    reason : 'Sell due to Time Stop',
+                    timestamp : price.timestamp,
+                };
+            }
+        }
+
+        if (!hasPosition && rsiValue < this.oversoldThreshold){
+            this.timeLeft = this.timeStop;
+
+            return {
+                symbol : this.symbol,
+                action : 'BUY',
+                confidence : (this.oversoldThreshold - rsiValue) / this.oversoldThreshold,
+                reason : 'RSI value under over sold threshold',
+                timestamp : price.timestamp,
+            }
+        }
+
+        return {
+            symbol : this.symbol,
+            action : 'HOLD',
+            confidence : 0,
+            reason : 'Waiting for favorable conditions to buy/sell',
+            timestamp : price.timestamp,
+        }
+    }
+
+    reset() : void {
+        this.RSI.reset();
+        this.timeLeft = this.timeStop;
     }
 }
